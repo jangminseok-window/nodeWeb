4140c09673a9ffb119891d8063860224f95bc74e
midstar <midstar.jang@gmail.com>
2024-10-02 09:40:41 +0900

    젠킨스 shell 추가 수정

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Node.js 애플리케이션 재시작
-pkill -f "node app.js"
-cd /path/to/your/app
-nohup node app.js > app.log 2>&1 &
+pkill -f "node promiseConn.js"
+cd /home/ubuntu/nodeWeb/mySql
+nohup node promiseConn.js > promiseConn.log 2>&1 &
 
 # 또는 Apache 재시작
 sudo systemctl restart apache2
