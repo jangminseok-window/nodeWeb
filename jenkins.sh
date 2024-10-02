a5d97a21ee600eed05d088060066a8ab8c5ce4cd
midstar <midstar.jang@gmail.com>
2024-10-02 15:45:26 +0900

    jenkins sh에 게시판 관련부분 추가

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Node.js 애플리케이션 재시작
-pkill -f "node promiseConn.js"
-cd /home/ubuntu/nodeWeb/mySql
-nohup /home/ubuntu/.nvm/versions/node/v22.9.0/bin/node promiseConn.js > promiseConn.log 2>&1 &
+pkill -f "node board.js"
+cd /home/ubuntu/nodeWeb
+nohup /home/ubuntu/.nvm/versions/node/v22.9.0/bin/node board.js > board.log 2>&1 &
 
 
 # 또는 Apache 재시작
