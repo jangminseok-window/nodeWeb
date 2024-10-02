9e72913a7b6a5915acb567a62cb5cb808185cd6e
midstar <midstar.jang@gmail.com>
2024-10-02 10:07:27 +0900

    node실행위치변경

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@
 # Node.js 애플리케이션 재시작
 pkill -f "node promiseConn.js"
 cd /home/ubuntu/nodeWeb/mySql
-nohup node promiseConn.js > promiseConn.log 2>&1 &
+nohup /home/ubuntu/.nvm/versions/node/v22.9.0/bin/node promiseConn.js > promiseConn.log 2>&1 &
 
 
 # 또는 Apache 재시작
